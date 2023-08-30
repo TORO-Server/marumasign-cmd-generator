5570f2ec5714ad8e6feacd570bafb6c88acdd99f
malken21 <Malken_Marumasa@outlook.jp>
2023-08-30 19:07:13 +0900

    コメント追加と関数の場所の整理

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+// すべてのリクエストパラメーター取得
+const urlParams = new URLSearchParams(window.location.search);
+
+// "v"という名前のリクエストパラメーター取得
+const version = urlParams.get("v")
+
 async function gencmd() {
 
+    // HTMLから 画像アドレス を取得
     let address = document.getElementById("address").value;
 
     // もし URLを短縮するかどうか のチェックボックスにチェックが入っているたら
     if (document.getElementById("toShortURL").checked) {
+        // https://is.gd/ の URL短縮APIを利用して 短縮URLを生成
         json = await get(`https://is.gd/create.php?format=json&url=${encodeURIComponent(address)}`);
+        // 送られてきた json に shorturl があるかどうか チェック
         if (json.shorturl) {
+            //もし あったら
+            //address を 短縮URLに置き換える
             address = json.shorturl
+            // ログを送信
             console.log(`URLを短縮しました: ${address}`)
         } else {
+            //もし なかったら
+            //エラーのログを送信
             console.error("URLを短縮できませんでした")
         }
     }
 
 
+    //---------- HTMLから値を取得 ---------- start
     const width = document.getElementById("width").value;
     const height = document.getElementById("width").value;
     const x = document.getElementById("x").value;
@@ -22,44 +37,15 @@ async function gencmd() {
     const rx = document.getElementById("rx").value;
     const ry = document.getElementById("ry").value;
     const rz = document.getElementById("rz").value;
+    //---------- HTMLから値を取得 ---------- end
 
 
+    // 看板に書かれる文字を生成して "signText" に代入
     const signText = createSignText(address, width, height, x, y, z, rx, ry, rz)
 
+    // giveコマンドを生成
     const command = toCommand(signText);
 
-    document.getElementById(
-        "generate"
-    ).innerHTML = command;
-}
-
-
-function get(url) {// Get リクエスト
-    return new Promise((resolve) => {
-        fetch(url)
-            .then(res => res.json())
-            .then(json => resolve(json))
-            .catch(() => resolve(undefined));
-    });
-}
-
-function createSignText(address, width, height, x, y, z, rx, ry, rz) {
-    const texts = [address, x, y, z, height, width, rx, ry, rz]
-    return texts.join("|")
-}
-
-function toCommand(signText) {
-
-    const maxLength = 15;
-
-    let texts = [];
-    for (let i = 0; i < signText.length; i += maxLength) {
-        texts.push(signText.substr(i, maxLength));
-    }
-
-    texts = texts.map(text => `'["${text}"]'`)
-
-    texts = texts.concat(Array(4).fill(`'[""]'`)).slice(0, 4)
-
-    return `/give @p minecraft:oak_sign{BlockEntityTag:{front_text:{messages:[${texts}]}}}`
+    // HTMLに 生成した give コマンドを表示
+    document.getElementById("generate").innerHTML = command;
 }
---
+++ utils.js
@@ -0,0 +1,38 @@
+// Get リクエスト
+// json を return する
+function get(url) {
+    return new Promise((resolve) => {
+        fetch(url)
+            .then(res => res.json())
+            .then(json => resolve(json))
+            .catch(() => resolve(undefined));
+    });
+}
+
+
+// アドレス、大きさ、位置、回転 のデータから
+// 看板に書かれる文字を生成して
+// それを return する
+function createSignText(address, width, height, x, y, z, rx, ry, rz) {
+    const texts = [address, x, y, z, height, width, rx, ry, rz]
+    return texts.join("|")
+}
+
+
+// giveコマンドに変換して
+// それを return する
+function toCommand(signText) {
+
+    const maxLength = 15;
+
+    let texts = [];
+    for (let i = 0; i < signText.length; i += maxLength) {
+        texts.push(signText.substr(i, maxLength));
+    }
+
+    texts = texts.map(text => `'["${text}"]'`)
+
+    texts = texts.concat(Array(4).fill(`'[""]'`)).slice(0, 4)
+
+    return `/give @p minecraft:oak_sign{BlockEntityTag:{front_text:{messages:[${texts}]}}}`
+}
